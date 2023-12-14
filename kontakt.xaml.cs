ab61f5e889f0ae28888e74a0c7351c7440bc1e4b
Adelkaa33 <151524382+Adelkaa33@users.noreply.github.com>
2023-12-14 16:15:34 +0300

    Update kontakt.xaml.cs

--- kontakt.xaml.cs
+++ kontakt.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace radik227ms2016
+namespace adel227ms2016
 {
     /// <summary>
     /// Логика взаимодействия для kontakt.xaml
